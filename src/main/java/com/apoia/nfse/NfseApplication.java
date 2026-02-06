618da47d6f2442b5f6430b2b381cb1caa878d789
rafaelsds <rafael.sds@live.com>
2026-02-06 13:32:47 -0300

    add

---
+++ src/main/java/com/apoia/nfse/NfseApplication.java
@@ -0,0 +1,13 @@
+package com.apoia.nfse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfseApplication.class, args);
+	}
+
+}
